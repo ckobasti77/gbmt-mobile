e1787ae32130106d0514b05cb67c6d464b45a6ff
Jovan Milojevic <jovanm028@gmail.com>
2024-07-04 11:21:30 -0700

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div id='pocetak' style={{ backgroundPosition: window.innerWidth < 1024 && '50% 50%' }} className=' text-white flex flex-col gap-y-12 justify-center items-start px-4 lg:px-36 bg-[url(/./assets/images/landing-img.png)] bg-no-repeat bg-cover h-screen w-full'>
+    <div id='pocetak' style={{ backgroundPosition: window.innerWidth < 1024 && '50% 50%' }} className=' text-white flex flex-col gap-y-12 justify-center items-center lg:items-start px-4 lg:px-36 bg-[url(/./assets/images/landing-img.png)] bg-no-repeat bg-cover h-screen w-full'>
         {/* <div className="absolute inset-0 w-full h-full backdrop-blur-sm lg:backdrop-blur-md"></div> */}
-        <h1 style={{textShadow: '1px 1px 10px #000',}} className='text-2xl lg:text-4xl font-black z-10'>Vaš Sledeći Pametni Telefon Vas Čeka!</h1>
-        <h2 className='z-10'>Istražite našu široku ponudu pametnih telefona i dodataka. <br /> Kvalitet, performanse i pristupačnost na jednom mestu.</h2>
+        <h1  className='text-2xl lg:text-6xl font-black z-10 w-full lg:w-1/2 text-center lg:text-left'>Vaš Sledeći Pametni Telefon Vas Čeka!</h1>
+        <h2 className='z-10 text-sm lg:text-xl text-center lg:text-left'>Istražite našu široku ponudu pametnih telefona i dodataka. <br /> Kvalitet, performanse i pristupačnost na jednom mestu.</h2>
+        <div className="absolute bottom-3 right-0 left-0 flex items-center justify-center">
+          <a
+            href="#ajfon15"
+            aria-label="Read more"
+            className="flex justify-center w-[25px] h-[45px] border-2 rounded-xl cursor-pointer mini-mouse hover:border-primary group"
+            data-aos="fade-up"
+            data-aos-delay="700"
+          >
+            <div className="bg-white rounded-full w-[5px] h-[8px] mt-2 mini-mouse-scroll group-hover:bg-primary"></div>
+          </a>
+        </div>
     </div>
   )
 }
